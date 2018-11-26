e6f53a9ff424b1eb8763f2c8d634cb5dd3851cd9
Michal Cylwik <michal@MichalPC.home>
2018-11-26 13:01:19 +0000

    Added navigation and regex.

--- app/src/main/java/com/example/anna/helloworld/MainActivity.kt
+++ app/src/main/java/com/example/anna/helloworld/MainActivity.kt
@@ -1,12 +1,24 @@
 package com.example.anna.helloworld
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    override fun onBackPressed() {
+        super.onBackPressed()
+        finish()
+    }
+
+    fun navigateSignUp(view: View) {
+        val newIntent: Intent = Intent(this, Register::class.java)
+        startActivity(newIntent)
     }
 }
--- app/src/main/java/com/example/anna/helloworld/Register.kt
+++ app/src/main/java/com/example/anna/helloworld/Register.kt
@@ -2,11 +2,38 @@ package com.example.anna.helloworld
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.EditText
+import android.widget.TextView
 
 class Register : AppCompatActivity() {
+
+    val editTextEmail: EditText by lazy {
+        findViewById<EditText>(R.id.editTextEmail)
+    }
+    val textViewOk: TextView by lazy {
+        findViewById<TextView>(R.id.textViewOk)
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register)
+    }
+
+    override fun onBackPressed() {
+        super.onBackPressed()
+        finish()
+    }
+
+    fun register(view: View) {
+        val regex = Regex(".*@.*[.].*")
+        if(regex.containsMatchIn(editTextEmail.text)) {
+            textViewOk.visibility = View.VISIBLE
+            textViewOk.text = "OK"
+        }
+        else {
+            textViewOk.visibility = View.VISIBLE
+            textViewOk.text = "Please input a correct email"
+        }
     }
 }
